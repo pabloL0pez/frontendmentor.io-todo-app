ff494f2cd66d368ba8587f08b665818f772a70d6
Pablo López <pablo.lopez3895@gmail.com>
2024-01-22 08:21:19 -0300

    fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
+import { fileURLToPath, URL } from "url";
 import react from '@vitejs/plugin-react'
-import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
     alias: [
-      { find: '', replacement: fileURLToPath(new URL('./src', import.meta.url)) },
       { find: 'components', replacement: fileURLToPath(new URL('./src/components', import.meta.url)) },
       { find: 'contexts', replacement: fileURLToPath(new URL('./src/contexts', import.meta.url)) },
       { find: 'assets', replacement: fileURLToPath(new URL('./src/assets', import.meta.url)) },
